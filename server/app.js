c2496c8fb720c15eae6b4dde6eae0cca5f356166
Mangesh Sangapu <msangapu@microsoft.com>
2024-05-13 13:34:03 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ const getRoles = (req, res) => {
 
   // Uncomment this code below and plug in your own alias to test the contributor role
   /*
-  if (body.userDetails === 'yourGoogleAlias@gmail.com') {
+  if (body.userDetails === 'get.mango@gmail.com') {
     res.send({
       "roles": [
         "contributor"
